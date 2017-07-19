0b44816db9428a793fd188a459c44f9755db66ac
f0zze <a.beinarovicsdeveloper@gmail.com>
2017-07-19 15:10:33 +0300

    Update tournament.sql

--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,37 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+CREATE TABLE Players (
+  id       SERIAL       NOT NULL,
+  fullname VARCHAR(120) NOT NULL UNIQUE,
+  
+  CONSTRAINT players_pkc PRIMARY KEY (id),
+  CONSTRAINT players_fullname_uniq UNIQUE (fullname)
+);
+
+INSERT INTO Players(fullname) VALUES ('Aigars B');
+SELECT COUNT(*) as registred FROM Players;
+
+CREATE TABLE Matches (
+  id     SERIAL  NOT NULL,
+  playe1 INTEGER NOT NULL,
+  playe2 INTEGER NOT NULL,
+  winner INTEGER,
+  CONSTRAINT id_pkc PRIMARY KEY (id),
+  CONSTRAINT player1_fkc FOREIGN KEY (player1) REFERENCES Players (id)
+  CONSTRAINT player2_fkc FOREIGN KEY (player2) REFERENCES Players (id)
+  CONSTRAINT winner_fkc  FOREIGN KEY (winner)  REFERENCES Players (id)
+);
+
+-- deleteMatches
+DELETE FROM Players;
+-- deletePlayers
+DELETE FROM Matches;
+
+-- countPlayers
+SELECT COUNT(*) as registered FROM PLAYERS;
+
+-- registerPlayer
+INSERT INTO Players(fullname) VALUES ('%Player fullname%')
+
 
